4c078994ba8ef8529241458364e06bc75ae2309d
Shokh Abbos Ulugmurodov <71746304+shohabbosdev@users.noreply.github.com>
2024-12-25 19:25:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,20 +11,35 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	# print(message.text)
 	try:
 		headers = {"Content-Type": "application/json"} 
 		data = {"text": message.text}
 		urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) 
 
 		response = requests.post(ttsurl, headers=headers, json=data, verify=False)
-
+		status_message = bot.reply_to(
+			message,
+			"✅ <b>Siz yuborgan matn qabul qilindi!!!\nBiroz qayta ishlab sizga 🔉 faylini yuboramiz...</b>"
+		)
 		if response.status_code==200:
-			# bot.send_message(response)
-			bot.send_audio(message.chat.id, response.content, reply_to_message_id=message.id, caption=f'<code>{message.text}</code>\n\n👉@ttsuzgenbot', title='@ttsuzgenbot', protect_content=True)
+			bot.send_audio(
+				message.chat.id, 
+				response.content, 
+				reply_to_message_id=message.id, 
+				caption= f'✍️ <pre>{message.text}</pre>\n\n👉@ttsuzgenbot', 
+				title='@ttsuzgenbot', 
+				protect_content=True
+			)
+			bot.delete_message(
+		            message.chat.id,
+		            status_message.message_id
+		        )
 		else:
-			bot.send_message(response.status_code)
+			bot.send_message(f'Kelgan xatolik: {response.status_code}')
 	except Exception as e:
-		bot.send_message(chat_id=message.chat.id,text=f"<u>Xatolik sodir bo'ldi:</u>\n<code>{e}</code>")
+		bot.send_message(
+			chat_id=message.chat.id,
+			text=f"<u>Xatolik sodir bo'ldi:</u>\n<code>{e}</code>"
+		)
 
 bot.infinity_polling()
