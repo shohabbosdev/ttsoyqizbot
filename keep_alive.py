506535b67702acaa8b849514cf7090363751da0b
Shokh Abbos Ulugmurodov <71746304+shohabbosdev@users.noreply.github.com>
2024-12-26 19:25:26 +0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -6,10 +6,10 @@ app =Flask(__name__)
 
 @app.route('/')
 def index():
-  return "TTSOYQIZBOT"
+  return "TTSOYQIZ ishlamoqda 1000 -portda"
 
 def run():
-  app.run(host='0.0.0.0',port=8088)
+  app.run(host='0.0.0.0',port=1000)
 
 def keep_alive():
   t=Thread(target=run)
